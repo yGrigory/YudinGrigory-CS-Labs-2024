c5fc31f3d14af7e319065280d4df94cb27358721
Григорий <grigorydin@gmail.com>
2024-10-13 20:33:42 +0300

    change main.cpp

--- main.cpp
+++ main.cpp
@@ -7,102 +7,99 @@ const int kTask1 = 1;
 const int kTask2 = 2;
 const int kTask3 = 3;
 const int kTask4 = 4;
-const int kFixedWidth = 12;
-const int kFixedPrecision = 8;
+
+const int kTask3WidthX = 12;
+const int kTask3WidthY = 12;
+const int kTask3WidthS = 12;
+const int kTask3WidthN = 4;
+
+const int kTask3PrecisionX = 1;
+const int kTask3PrecisionY = 8;
+const int kTask3PrecisionS = 8;
+const int kTask3PrecisionN = 0;
 }  // namespace
 
 void Task1() {
     int n = {};
     int m = {};
-    int numsSum = 0;
-    const int divider = 5;
-    const int rangeStart = 1;
 
     std::cout << "Введите натуральные чила n, m, притом m < n: ";
     std::cin >> n >> m;
 
-    for (int numsIterator = rangeStart; numsIterator <= n; ++numsIterator) {
-        if (numsIterator % divider == 0 && numsIterator % m != 0) {
-            numsSum += numsIterator;
+    if (m >= n || n < 1 || m < 1) {
+        std::cout << "Некорректный ввод" << std::endl;
+        return;
+    }
+
+    const int step = 5;
+    const int rangeStart = 5;
+    int sum = 0;
+
+    for (int i = rangeStart; i <= n; i += step) {
+        if (i % m != 0) {
+            sum += i;
         }
     }
 
-    std::cout << numsSum << std::endl;
+    std::cout << "Сумма: " << sum << std::endl;
 }
 
 void Task2() {
-    const int rangePozitiveAStart = 2;
+    const int rangeNotNegativeAStart = 2;
+    const int rangeNotNegativeAEnd = 8;
     const int rangeNegativeAStart = 3;
-    const int rangePozitiveAEnd = 8;
     const int rangeNegativeAEnd = 9;
+
     double a = {};
-    double numsProduct = 1;
 
     std::cout << "Введите число a: ";
     std::cin >> a;
 
+    double product = 1.0;
     if (a >= 0) {
-        for (int numsIterator = rangePozitiveAStart; numsIterator <= rangePozitiveAEnd; numsIterator += 2) {
-            numsProduct *= pow(numsIterator, 2);
+        for (int i = rangeNotNegativeAStart; i <= rangeNotNegativeAEnd; i += 2) {
+            product *= (i * i) - a;
         }
-        numsProduct -= a;
+        product -= a;
     } else {
-        for (int numsIterator = rangeNegativeAStart; numsIterator <= rangeNegativeAEnd; numsIterator += 3) {
-            numsProduct *= (numsIterator - 2);
+        for (int i = rangeNegativeAStart; i <= rangeNegativeAEnd; i += 3) {
+            product *= (i - 2);
         }
     }
 
-    std::cout << numsProduct << std::endl;
+    std::cout << "Произведение: " << product << std::endl;
 }
 
-void CalculateTaylorRow(double x) {
-    const double delta = 1e-6;
-    double y = log((1 + x) / (1 - x));
+void CalculateTaylorSeries(double x) {
+    const double epsilon = 1e-6;
+    double y = std::log((1 + x) / (1 - x));
     int n = 1;
-    double s = x;
-    double prevElem = x;
-    double curElem = x;
+    double s = 2 * x;
+    double prevSeriesElem = 2 * x;
 
-    while (2 * curElem > delta) {
-        double k = pow(x, 2) * (2 * n - 1) / (2 * n + 1);
-        curElem = prevElem * k;
-        prevElem = curElem;
-        s += curElem;
-        n++;
+    while (prevSeriesElem > epsilon) {
+        double k = x * x * (2 * n - 1) / (2 * n + 1);
+        prevSeriesElem *= k;
+        s += prevSeriesElem;
+        ++n;
     }
 
-    std::cout << std::setw(kFixedWidth) << x << std::setw(kFixedWidth) << std::setprecision(kFixedPrecision) << y << std::setw(kFixedWidth)
-              << std::setprecision(kFixedPrecision) << 2 * s << std::setw(kFixedWidth) << n << std::endl;
+    std::cout << std::setw(kTask3WidthX) << std::fixed << std::setprecision(kTask3PrecisionX) << x << std::setw(kTask3WidthY)
+              << std::setprecision(kTask3PrecisionY) << y << std::setw(kTask3WidthS) << std::setprecision(kTask3PrecisionS) << s
+              << std::setw(kTask3WidthN) << n << std::endl;
 }
 
 void Task3() {
     const double h = 0.2;
-    const int rangeEnd = 1;
+    const double maxH = 0.8;
     double x = 0;
 
-    std::cout << std::setw(kFixedWidth) << "x" << std::setw(kFixedWidth) << "Y" << std::setw(kFixedWidth) << "s" << std::setw(kFixedWidth) << "n"
+    std::cout << std::setw(kTask3WidthX) << "x" << std::setw(kTask3WidthY) << "Y" << std::setw(kTask3WidthS) << "s" << std::setw(kTask3WidthN) << "n"
               << std::endl;
 
-    while (x < rangeEnd) {
-        CalculateTaylorRow(x);
+    while (x <= maxH) {
+        CalculateTaylorSeries(x);
         x += h;
-    }
-}
-
-void CalculateY(double n, double x) {
-    const int n1 = 3;
-    const int n2 = 5;
-    const int n3 = 10;
-
-    int rangeStart = 1;
-    double y = x;
-
-    for (int numsIterator = rangeStart; numsIterator <= n; numsIterator++) {
-        y += (pow(x, numsIterator + 1)) / (numsIterator * pow(2, numsIterator));
-        if ((numsIterator == n1) || (numsIterator == n2) || (numsIterator == n3) ||
-            (numsIterator == n && numsIterator != n1 && numsIterator != n2 && numsIterator != 3)) {
-            std::cout << "Для n = " << numsIterator << ", Y = " << y << std::endl;
-        }
     }
 }
 
@@ -116,25 +113,29 @@ void Task4() {
     const int n1 = 3;
     const int n2 = 5;
     const int n3 = 10;
-
-    int rangeStart = 1;
+    const int rangeStart = 1;
     double y = x;
-    for (int numsIterator = rangeStart; numsIterator <= n; numsIterator++) {
-        y += (pow(x, numsIterator + 1)) / (numsIterator * pow(2, numsIterator));
-        if ((numsIterator == n1) || (numsIterator == n2) || (numsIterator == n3) ||
-            (numsIterator == n && numsIterator != n1 && numsIterator != n2 && numsIterator != n3)) {
-            std::cout << "Для n = " << numsIterator << ", Y = " << y << std::endl;
+
+    for (int i = rangeStart; i <= n; ++i) {
+        y += (pow(x, i + 1)) / (i * pow(2, i));
+        if (i == n1 || i == n2 || i == n3 || i == n) {
+            std::cout << "Для n = " << i << ", Y = " << y << std::endl;
         }
     }
 }
 
-int main() {
-    char workStatus = 'y';
-    while (workStatus == 'y') {
-        int userChoice{};
+int main(int, char**) {
+    char continueExecution = 'y';
+    while (continueExecution == 'y') {
+        std::cout << "Задача 1 - поиск суммы на отрезке" << std::endl;
+        std::cout << "Задача 2 - вычисление произведения чисел на отрезке" << std::endl;
+        std::cout << "Задача 3 - вычисление ряда Тейлора" << std::endl;
+        std::cout << "Задача 4 - вычисление Y по формуле" << std::endl;
         std::cout << "Введите номер задачи от 1 до 4: ";
-        std::cin >> userChoice;
-        switch (userChoice) {
+
+        int task{};
+        std::cin >> task;
+        switch (task) {
             case 1:
                 Task1();
                 break;
@@ -152,6 +153,6 @@ int main() {
         }
 
         std::cout << "Хотите продолжить работу (y - продолжить, n - завершить)? ";
-        std::cin >> workStatus;
+        std::cin >> continueExecution;
     }
 }
