cf2b8c623ac9c957bcdbbed00f6f8051e6cb0989
Григорий <grigorydin@gmail.com>
2024-10-21 22:07:46 +0300

    last change lab_3

--- methods.h
+++ methods.h
@@ -4,24 +4,24 @@ struct FunctionResult {
 };
 
 namespace Menu {
-    enum class Option {
-        Option1,
-        Option2,
-        Option3,
-    };
+enum class Option {
+    Option1,
+    Option2,
+    Option3,
+};
 
-    void start();
-    void inputProcessing(Option option);
+void start();
+void inputProcessing(Option option);
 }  // namespace Menu
 
 namespace Run {
-    void RunIterationMethod();
-    void RunNewtonsMethod();
-    void RunHalfCoefficientMethod();
+void RunIterationMethod();
+void RunNewtonsMethod();
+void RunHalfCoefficientMethod();
 }  // namespace Run
 
 namespace Calculation {
-    [[nodiscard]] FunctionResult CalculateIterationMethod(double epsilon, double coefficient);
-    [[nodiscard]] FunctionResult CalculateNewtonsMethod(double initialApproximation, double epsilon, double coefficient);
-    [[nodiscard]] FunctionResult CalculateHalfCoefficientMethod(double leftRangeLimit, double rightRangeLimit, double epsilon, double coefficient);
+[[nodiscard]] FunctionResult CalculateIterationMethod(double epsilon, double coefficient);
+[[nodiscard]] FunctionResult CalculateNewtonsMethod(double initialApproximation, double epsilon, double coefficient);
+[[nodiscard]] FunctionResult CalculateHalfCoefficientMethod(double leftRangeLimit, double rightRangeLimit, double epsilon, double coefficient);
 }  // namespace Calculation
